cb16185998c03db2095f4634831645088e8a457c
Gregory Ruiz-Ade <gregory.ruiz-ade@activenetwork.com>
2015-06-01 08:38:13 -0700

    Style/Rubocop fixes

--- lib/vcoworkflows/config.rb
+++ lib/vcoworkflows/config.rb
@@ -16,6 +16,8 @@ module VcoWorkflows
     # Whether or not to do SSL/TLS Certificate verification
     attr_accessor :verify_ssl
 
+    # rubocop:disable LineLength, MethodLength, CyclomaticComplexity, PerceivedComplexity
+
     # Constructor
     # @param [String] config_file Path to config file to load
     # @param [String] url URL for vCO server
@@ -24,7 +26,6 @@ module VcoWorkflows
     # @param [Boolean] verify_ssl Verify SSL Certificates?
     # @return [VcoWorkflows::Config]
     def initialize(config_file: nil, url: nil, username: nil, password: nil, verify_ssl: true)
-
       # If we're given a URL and no config_file, then build the configuration
       # from the values we're given (or can scrape from the environment).
       # Otherwise, load the given config file or the default config file if no
@@ -41,25 +42,24 @@ module VcoWorkflows
       end
 
       # If the uri didn't specify the /vco/api path, add it...
-      @url.gsub!(%r{\/$}, '/vco/api') unless @url.match(/\/vco\/api$/)
+      @url.gsub!(%r{\/$}, '/vco/api') unless @url.match(%r{/vco/api$})
 
       # Fail if we don't have both a username and a password.
       fail(IOError, ERR[:url_unset]) if @url.nil?
       fail(IOError, ERR[:username_unset]) if @username.nil?
       fail(IOError, ERR[:password_unset]) if @password.nil?
-
     end
+    # rubocop:enable LineLength, MethodLength, CyclomaticComplexity, PerceivedComplexity
 
     # load config file
     # @param [String] config_file Path for the configuration file to load
     def load_config(config_file)
       config_data = JSON.parse(File.read(config_file))
-      unless config_data.nil?
-        @url        = config_data['url']
-        @username   = config_data['username']
-        @password   = config_data['password']
-        @verify_ssl = config_data['verify_ssl']
-      end
+      return if config_data.nil?
+      @url        = config_data['url']
+      @username   = config_data['username']
+      @password   = config_data['password']
+      @verify_ssl = config_data['verify_ssl']
     end
 
     # Return a String representation of this object
